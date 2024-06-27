1df9b2c347fe9579305761507cf232ad87386703
Vipin <vpnbeniwal123@gmail.com>
2024-06-27 17:28:06 +0530

    fixedEmailBtn

--- client/src/components/SearchResults/ResultCard.jsx
+++ client/src/components/SearchResults/ResultCard.jsx
@@ -96,7 +96,7 @@ const ResultCard = () => {
           Virgin Islands of the United States Real Estate & Homes for Sale
         </h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <div key={product.id}>
           <div className="text-xs">
@@ -110,7 +110,7 @@ const ResultCard = () => {
                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                   />
                 </div>
-                <div className="mt-4 flex justify-between p-4">
+                <div className="mt-4 flex justify-between  p-4">
                   <div>
                     <h3 className="text-sm text-gray-700">
                       <a
@@ -127,24 +127,24 @@ const ResultCard = () => {
                     <h1 className="text-2xl font-semibold my-2">
                       {product.price}
                     </h1>
-                    <div className="flex">
-                      <p className="mt-1 text-sm text-gray-900 mr-2 ">
+                    <div className="flex text-xs md:text-sm">
+                      <p className="mt-1  text-gray-900 mr-2 ">
                         <span className="font-semibold">{product.bed}</span> bed
                       </p>
-                      <p className="mt-1 text-sm text-gray-900 mx-2 ">
+                      <p className="mt-1  text-gray-900 mx-2 ">
                         <span className="font-semibold">{product.bath}</span>{" "}
                         bed
                       </p>
-                      <p className="mt-1 text-sm text-gray-900 ml-2 ">
+                      <p className="mt-1  text-gray-900 ml-2 ">
                         <span className="font-semibold">{product.area}</span>{" "}
                         bed
                       </p>
                     </div>
-                    <p className="mt-1 text-sm text-gray-900 w-2/3 ">
-                      {product.address}{" "}
+                    <p className="mt-1  text-xs md:text-sm text-gray-900 2xl:w-2/3 ">
+                      {product.address}
                     </p>
                   </div>
-                  <div className="text-sm  flex items-end font-medium text-gray-900 ">
+                  <div className="md:text-sm text-xs  flex items-end justify-end font-medium text-gray-900 w-1/2">
                     <div className="px-2 py-2 border border-black rounded-full">
                       {product.agent}
                     </div>
